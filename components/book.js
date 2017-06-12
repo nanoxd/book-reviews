f46bcad845d2c10745c006eab27949d88f387647
Fernando Paredes <nano@fdp.io>
2017-06-11 21:04:03 -0700

    [book] Use BEM naming for all items

--- components/book.js
+++ components/book.js
@@ -1,33 +1,32 @@
 import Link from 'next/link'
 
 export default ({ title, imageUrl, author, slug }) =>
-  <div key={slug}>
+  <div key={slug} className='Book'>
     <Link href={`/review?slug=${slug}`} as={`/review/${slug}`}>
-      <a>
-
-        {imageUrl && <img src={imageUrl} />}
-        <h3>{title}</h3>
+      <a className='Book--link'>
+        {imageUrl && <img src={imageUrl} className='Book--image' />}
+        <h3 className='Book--title'>{title}</h3>
       </a>
     </Link>
     <style jsx>{`
-      div {
+      .Book {
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
       }
 
-      a {
+      .Book--link {
         text-decoration: none;
         color: black;
         text-align: center;
       }
 
-      img {
+      .Book--image {
         height: 18rem;
       }
 
-      h3 {
+      .Book--title {
         font-size: 1rem;
         text-align: center;
       }
